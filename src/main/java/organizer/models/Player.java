2c93aa0508d1b5189d0e6c1b4cf6451eb2040c09
AErdenberger <erdenberger.alexander@gmail.com>
2024-04-26 19:12:48 -0700

    Player Class Built

--- src/main/java/organizer/models/Player.java
+++ src/main/java/organizer/models/Player.java
@@ -4,5 +4,50 @@ public class Player{
     private String name;
     private String ingame;
     private Rank Rank;
+    private Role Primary;
+    private Role Secondary;
+
+    public Player(String name, String ingame, Rank rank, Role primary, Role secondary) {
+        this.name = name;
+        this.ingame = ingame;
+        Rank = rank;
+        Primary = primary;
+        Secondary = secondary;
+    }
+
+    public Player(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIngame() {
+        return ingame;
+    }
+
+    public Rank getRank() {
+        return Rank;
+    }
+
+    public Role getPrimary() {
+        return Primary;
+    }
+
+    public Role getSecondary() {
+        return Secondary;
+    }
+
+    public java.lang.String toString() {
+        final java.lang.StringBuilder sb = new java.lang.StringBuilder("Player{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", ingame='").append(ingame).append('\'');
+        sb.append(", Rank=").append(Rank);
+        sb.append(", Primary=").append(Primary);
+        sb.append(", Secondary=").append(Secondary);
+        sb.append('}');
+        return sb.toString();
+    }
 }
 
