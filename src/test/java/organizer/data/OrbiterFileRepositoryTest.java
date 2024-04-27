079e91294c21fcc33bd8097150eb42e7d0962fe8
AErdenberger <erdenberger.alexander@gmail.com>
2024-04-27 15:28:11 -0700

    begin test writing

---
+++ src/test/java/organizer/data/OrbiterFileRepositoryTest.java
@@ -0,0 +1,19 @@
+package organizer.data;
+
+import organizer.models.Role;
+import organizer.models.Rank;
+import organizer.models.Player;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrbiterFileRepositoryTest {
+}
